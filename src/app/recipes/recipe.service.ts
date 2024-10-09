5342df1524c8c180c9b17c1ec469d268cf024577
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-10-09 12:30:04 -0600

    Se migro de eventos personalizados a un servicio para el componente de recipes.

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,15 @@
+import { EventEmitter } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+    
+    private recipes : Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'),
+    ];
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+}
