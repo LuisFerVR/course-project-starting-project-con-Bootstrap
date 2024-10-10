a467ca6669b275c67e14e558b861fa263515c089
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-10-10 15:33:50 -0600

    Se agregó el sustituyó EventEmitter en los servicios por Subject y el método next. Se eliminaron los método que pasaban el id de la receta, ya que ahora lo hacemos por la URL.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature='recipe';
-  ingredients=[];
-  constructor(){}
-  onNavigate(feature:string){
-    this.loadedFeature=feature;
-  }
+  
 }
