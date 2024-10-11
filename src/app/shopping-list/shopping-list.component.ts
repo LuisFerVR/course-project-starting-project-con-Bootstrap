87c9f0c0fddb9d3e0a04fbe6f17fc0136087a1d5
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-10-11 11:17:41 -0600

    Se agregaron los métodos necesarios y observables como subject para poder actualizar un ingrediente, aparte de crear uno nuevo como ya se tenia. Shopping-list y shopping-edit components.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -23,4 +23,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy{
   ngOnDestroy(): void {
     this.ingredientsSubscribtion.unsubscribe();
   }
+
+  onEditItem(index:number){
+    this.shoppingListService.startedEditing.next(index);
+  }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import { Subject } from "rxjs";
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
     private ingredients : Ingredient[] = [
         new Ingredient('Apple', 5),
         new Ingredient('Tomato', 10)
@@ -12,8 +13,17 @@ export class ShoppingListService {
         return this.ingredients.slice();
     }
 
+    getIngredient(index:number) {
+        return this.ingredients[index];
+    }
+
     addIngredients(ingredient:Ingredient){
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
+    updateIngredient(index:number, newIngredient:Ingredient){
+        this.ingredients[index] = newIngredient;
         this.ingredientsChanged.next(this.ingredients.slice());
     }
 
