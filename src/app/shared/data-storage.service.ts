965c3fd11469d541a9bc181ddfb6d5edc0ac2202
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-10-15 09:17:02 -0600

    Se agregó el servicio a la aplicación como proveedor y se agregó el "@" al decorador del servicio.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 
-Injectable({providedIn: 'root'})
+@Injectable({providedIn: 'root'})
 export class DataStorageService {
     constructor(private http:HttpClient,private recipeService:RecipeService) {}
 
