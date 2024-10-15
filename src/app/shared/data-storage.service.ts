6dda1b29e4f7ebdf2761800a463e8c508b218593
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2024-10-15 09:08:28 -0600

    Se conectó la aplicacióna  una BD firebase y agregaron los métodos en un servicio para poder almacenar las recetas y traerlas de regreso a la plaicación.

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+
+Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor(private http:HttpClient,private recipeService:RecipeService) {}
+
+    storeRecipes(){
+        const recipes = this.recipeService.getRecipes();
+        this.http.put('https://ng-course-recipe-book-80edc-default-rtdb.firebaseio.com/recipies.json', recipes).subscribe(response=>{
+            console.log(response);
+        });
+    }
+
+    fetchRecipes(){
+        this.http.get<Recipe[]>('https://ng-course-recipe-book-80edc-default-rtdb.firebaseio.com/recipies.json').subscribe(recipes=>{
+            this.recipeService.setRecipes(recipes);
+        });
+    }
+}
